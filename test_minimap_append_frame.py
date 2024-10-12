b55eb5144fa80282c4183553004ce57d7b35e850
hooved <172129504+hooved@users.noreply.github.com>
2024-10-12 09:38:22 -0400

    stitch multiple frames together by appending

--- test_minimap_append_frame.py
+++ test_minimap_append_frame.py
@@ -5,12 +5,14 @@ from training_data import crop_frame
 
 if __name__=="__main__":
   frame_1 = crop_frame(np.array(Image.open("data/train/3/2/0.png")), 600)
-  frame_2 = crop_frame(np.array(Image.open("data/train/3/2/1.png")), 600)
   minimap = frame_1
   origin = (600, 600)
   last_position = origin
-  diff_frames = np.array([frame_1, frame_2])
 
-  minimap, origin, last_position = minimap_append_frame(minimap, diff_frames, origin, last_position)
-  Image.fromarray(minimap).save("data/train/3/2/0_1.png")
+  for i in range(1, 4):
+    frame_1 = crop_frame(np.array(Image.open(f"data/train/3/2/{i-1}.png")), 600)
+    frame_2 = crop_frame(np.array(Image.open(f"data/train/3/2/{i}.png")), 600)
+    diff_frames = np.array([frame_1, frame_2])
+    minimap, origin, last_position = minimap_append_frame(minimap, diff_frames, origin, last_position)
+    Image.fromarray(minimap).save(f"data/train/3/2/0_{i}.png")
   done = 1

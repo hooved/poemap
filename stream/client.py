1e55e5ea3b65ad10d2e7969cefec6c9ca48a8820
hooved <172129504+hooved@users.noreply.github.com>
2024-10-14 09:21:26 -0400

    async produce and display stitched minimap frames

--- stream/client.py
+++ stream/client.py
@@ -1,68 +1,72 @@
-#import pyautogui
-#import keyboard
-import time, os, sys, socket
-from .comms import send_array
+import pyautogui
+import keyboard
+import time, os, sys, socket, asyncio
+from comms import send_array
 import numpy as np
 import cv2
 from PIL import Image
 
-def capture_screen():
+class AsyncState:
+  def __init__(self):
+    self.data = None
+    self.lock = asyncio.Lock()
+    self.data_ready = asyncio.Event()
+
+  async def write(self, data):
+    async with self.lock:
+      self.data = data
+      self.data_ready.set()
+
+  async def read(self):
+    await self.data_ready.wait()
+    async with self.lock:
+      self.data_ready.clear()
+      return self.data
+
+def capture_screen(box_radius):
   # player icon is at 1920, 1060, in 4k
-  #box_radius = 300
-  region = (1620, 760, 600, 600)
+  region = (1920 - box_radius, 1060 - box_radius, box_radius*2, box_radius*2)
   screenshot = pyautogui.screenshot(region=region)
   return np.array(screenshot)
-  #return screenshot.save("test.png")
-  #img_byte_arr = io.BytesIO()
-  #screenshot.save(img_byte_arr, format='JPEG', quality=50)  # Compress to JPEG
-  #screenshot.save(img_byte_arr, format='PNG', compress_level=0)
-  #return img_byte_arr.getvalue()
 
-def stream_frames(target_fps=2):
+async def produce_minimap(state: AsyncState, target_fps=2, box_radius=600):
+  global stop_stream, stop_program
   frame_time = 1 / target_fps
-  frames = []
+  minimap = capture_screen(box_radius)
+  frames = [minimap]
+  origin = (box_radius, box_radius)
+  last_position = origin
   print("Starting stream...")
   while True:
     last_capture_time = time.perf_counter()
-    frame = capture_screen()
+    frame = capture_screen(box_radius)
     frames.append(frame)
-    print(f"{len(frames)} frames")
-
-    if len(frames) == 32:
-      stop_stream = True
-
-    if len(frames) % 8 == 0:
-      minimap = frames_to_map(np.array(frames))
-      #mask = map_to_mask(minimap)
-      cv2.namedWindow('Overlay', cv2.WINDOW_NORMAL)
-      cv2.setWindowProperty('Overlay', cv2.WND_PROP_TOPMOST, 1)
-      #cv2.moveWindow('Overlay', 3840-minimap.shape[1], 700)
-      cv2.imshow('Overlay', minimap)
-      cv2.waitKey(1)
+    minimap, origin, last_position = minimap_append_frame(minimap, np.array(frames), origin, last_position)
+    frames.pop(0)
+    await state.write(minimap)
 
     if stop_stream or stop_program:
       print("Stopping stream...")
-      cv2.destroyAllWindows()
-      #print(f"number of frames: {len(frames)}")
-      #minimap = frames_to_map(np.array(frames))
-      #Image.fromarray(minimap).save("minimap.png")
       return
 
     elapsed = time.perf_counter() - last_capture_time
     if elapsed < frame_time:
-      time.sleep(frame_time - elapsed)
+      await asyncio.sleep(frame_time - elapsed)
 
-def frames_to_map(frames):
-  moves = get_moves(frames)
-  return draw_minimap(frames, moves)
-
-def get_moves(frames):
-  moves = [(0,0)]
-  for i, frame in enumerate(frames[1:]):
-      move = find_translation(frames[i], frame)
-      moves.append(move)
-  moves = np.round(np.array(moves)).astype(int)
-  return moves
+async def consume_minimap(state: AsyncState):
+  global stop_stream, stop_program
+  while True:
+    minimap = await state.read()
+    #minimap = Image.fromarray(minimap)
+    cv2.namedWindow('Overlay', cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty('Overlay', cv2.WND_PROP_TOPMOST, 1)
+    #cv2.moveWindow('Overlay', 3840-minimap.shape[1], 700)
+    cv2.imshow('Overlay', minimap)
+    cv2.waitKey(1)
+    if stop_stream or stop_program:
+      cv2.destroyAllWindows()
+      return
+    await asyncio.sleep(3)
 
 # From https://github.com/kweimann/poe-learning-layouts/blob/main/utils/data.py, MIT License
 def find_translation(image_a, image_b, threshold=0.7, max_matches=15):
@@ -95,24 +99,6 @@ def find_translation(image_a, image_b, threshold=0.7, max_matches=15):
   dx, dy = np.median(points_a - points_b, axis=0)
   return dx, dy
 
-# From https://github.com/kweimann/poe-learning-layouts/blob/main/utils/data.py, MIT License
-def draw_minimap(video, movement):
-  current_position = movement.cumsum(axis=0)
-  x_min, y_min = current_position.min(axis=0)
-  x_max, y_max = current_position.max(axis=0)
-  assert (x_min <= 0) and (x_max >= 0)
-  assert (y_min <= 0) and (y_max >= 0)
-  (x_start, y_start) = abs(x_min), abs(y_min)
-  _, H_frame, W_frame, C = video.shape  # frame size
-  (H, W) = (y_max - y_min + H_frame, x_max - x_min + W_frame)  # map size
-  H += H % 2  # make height divisible by 2
-  W += W % 2  # make width divisible by 2
-  minimap = np.zeros((H, W, C), dtype=np.uint8)
-  for frame, (x, y) in zip(video, current_position):
-    minimap[y_start + y:y_start + y + H_frame, x_start + x:x_start + x + W_frame] = frame
-  origin = (y_start + H_frame // 2, x_start + W_frame // 2)
-  return minimap, origin
-
 def minimap_append_frame(minimap, diff_frames, origin, last_position):
   assert len(diff_frames) == 2
   _, H_frame, W_frame, C = diff_frames.shape  # frame size
@@ -140,6 +126,9 @@ def minimap_append_frame(minimap, diff_frames, origin, last_position):
   minimap[frame_top : frame_top+H_frame, frame_left : frame_left+W_frame] = diff_frames[1]
   return minimap, origin, current_position
 
+stop_stream = True
+stop_program = False
+
 def trigger_start_stream():
   global stop_stream
   stop_stream = False
@@ -152,26 +141,32 @@ def trigger_stop_program():
   global stop_program
   stop_program = True
 
-if __name__=="__main__":
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect(('localhost', 50000))
-  array = np.array(Image.open("minimap.png"))
-  send_array(client, array)
-  client.close()
-  sys.exit()
 
+async def main():
   # using global vars because keyboard doesn't allow triggering one hotkey's callback during another
-  stop_stream = True
-  stop_program = False
   keyboard.add_hotkey("ctrl+alt+q", trigger_start_stream)
   keyboard.add_hotkey("ctrl+alt+w", trigger_stop_stream)
   keyboard.add_hotkey("ctrl+alt+e", trigger_stop_program)
+  state = AsyncState()
 
   while True:
     if not stop_stream:
-      stream_frames(target_fps=2)
+      async with asyncio.TaskGroup() as tg:
+        producer = tg.create_task(produce_minimap(state, target_fps=1))
+        consumer = tg.create_task(consume_minimap(state))
+      #stream_frames(target_fps=1)
 
     if stop_program:
       sys.exit()
 
     time.sleep(0.5)
+
+if __name__=="__main__":
+  #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #client.connect(('localhost', 50000))
+  #array = np.array(Image.open("minimap.png"))
+  #send_array(client, array)
+  #client.close()
+  #sys.exit()
+
+  asyncio.run(main())

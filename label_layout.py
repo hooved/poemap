003d9d8dba745a5c7e6253e2ebbffac8c95ba0ec
hooved <172129504+hooved@users.noreply.github.com>
2024-10-18 12:28:23 -0400

    get stats for collected layout training data

--- label_layout.py
+++ label_layout.py
@@ -3,10 +3,16 @@ import os, sys, shutil, glob
 if __name__=="__main__":
   temp_dir = os.path.join("data", "train", "collect")
   layout_id = sys.argv[1]
-  os.makedirs(layout_dir := os.path.join("data", "train", layout_id), exist_ok=True)
-  current_instances = tuple(int(x) for x in os.listdir(layout_dir))
-  instance_id = 0 if not current_instances else max(current_instances) + 1
-  # Masks were inspected for quality, no longer needed for training
-  #for file in glob.glob(os.path.join(temp_dir, "*_mask.png")):
-    #os.remove(file)
-  shutil.move(temp_dir, os.path.join(layout_dir, str(instance_id)))
+  if layout_id == "c": # count how many instances have been collected per layout_id
+    layouts = os.listdir(os.path.join("data", "train"))
+    #layouts = dict(zip(layouts, [0]*len(layouts)))
+    for layout in layouts:
+      print(f"{layout}: {len(os.listdir(os.path.join('data', 'train', layout)))}")
+  else:
+    os.makedirs(layout_dir := os.path.join("data", "train", layout_id), exist_ok=True)
+    current_instances = tuple(int(x) for x in os.listdir(layout_dir))
+    instance_id = 0 if not current_instances else max(current_instances) + 1
+    # Masks were inspected for quality, no longer needed for training
+    #for file in glob.glob(os.path.join(temp_dir, "*_mask.png")):
+      #os.remove(file)
+    shutil.move(temp_dir, os.path.join(layout_dir, str(instance_id)))

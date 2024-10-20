3b4ed4c4807f083739ed43651522ece72e31f9e6
hooved <172129504+hooved@users.noreply.github.com>
2024-10-20 10:36:10 -0400

    save only single minimap of entire layout

--- label_layout.py
+++ label_layout.py
@@ -9,10 +9,15 @@ if __name__=="__main__":
     for layout in layouts:
       print(f"{layout}: {len(os.listdir(os.path.join('data', 'train', layout)))}")
   else:
+    minimap_id = sys.argv[2]
     os.makedirs(layout_dir := os.path.join("data", "train", layout_id), exist_ok=True)
     current_instances = tuple(int(x) for x in os.listdir(layout_dir))
     instance_id = 0 if not current_instances else max(current_instances) + 1
     # Masks were inspected for quality, no longer needed for training
     #for file in glob.glob(os.path.join(temp_dir, "*_mask.png")):
       #os.remove(file)
-    shutil.move(temp_dir, os.path.join(layout_dir, str(instance_id)))
+    os.mkdir(dest_dir := os.path.join(layout_dir, str(instance_id)))
+    for file in glob.glob(os.path.join(temp_dir, f"{minimap_id}*")):
+      shutil.move(file, dest_dir)
+    #shutil.move(temp_dir, os.path.join(layout_dir, str(instance_id)))
+    shutil.rmtree(temp_dir)

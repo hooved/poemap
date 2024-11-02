7ae33a69d4f8c3a6152a344d5478339d64cfcdfe
hooved <172129504+hooved@users.noreply.github.com>
2024-11-02 08:50:24 -0400

    stream with updated tokenization

--- stream/server.py
+++ stream/server.py
@@ -5,7 +5,7 @@ import asyncio, os
 from models import AttentionUNet, ViT
 from tinygrad.nn.state import safe_load, load_state_dict
 from tinygrad import dtypes, Tensor, TinyJit
-from training_data import extract_map_features, get_patches, get_tokens
+from training_data import extract_map_features, get_patches, get_tokens, tokenize_minimap
 from functools import partial
 
 async def minimap_to_layout(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, models: dict):
@@ -18,21 +18,20 @@ async def minimap_to_layout(reader: asyncio.StreamReader, writer: asyncio.Stream
     if header == ClientHeader.PROCESS_MINIMAP:
       try:
         minimap = await receive_array(reader, dtype=np.uint8)
-        original_origin = await receive_array(reader, dtype=np.uint32)
-        origin = tuple(int(x) for x in original_origin)
-        mask, origin = extract_map_features(minimap, origin, models["UNet"])
+        origin = await receive_array(reader, dtype=np.uint32)
+        tokens, mask = tokenize_minimap(minimap, origin, models["UNet"])
         # if mask is blank, will get error later on in ViT
         if np.any(mask != 0):
-          tokens = [get_tokens(get_patches(mask, origin))]
           if os.getenv("COLLECT"):
             save_dir = os.path.join("data", "train", "collect")
             os.makedirs(save_dir, exist_ok=True)
             Image.fromarray(minimap).save(os.path.join(save_dir, f"{timestamp}.png"))
-            np.savez_compressed(os.path.join(save_dir, f"{timestamp}_origin.npz"), data=original_origin)
+            np.savez_compressed(os.path.join(save_dir, f"{timestamp}_origin.npz"), data=origin)
             Image.fromarray(mask * 255, mode="L").save(os.path.join(save_dir, f"{timestamp}_mask.png"))
             np.savez_compressed(os.path.join(save_dir, f"{timestamp}.npz"), data=tokens)
           timestamp += 1
-          layout_id = models["ViT"](tokens)[0].argmax().cast(dtypes.uint8).item()
+          print(tokens.shape)
+          layout_id = models["ViT"]([tokens])[0].argmax().cast(dtypes.uint8).item()
           writer.write(layout_id.to_bytes(4, byteorder="big"))
           await writer.drain()
         else:
@@ -62,12 +61,13 @@ async def minimap_to_layout(reader: asyncio.StreamReader, writer: asyncio.Stream
 
 async def run_server(hostname, port: int):
   models = {
-    "UNet": AttentionUNet("AttentionUNet_4").load(),
-    "ViT": ViT("ViT2", num_classes=9, max_tokens=128, layers=3, embed_dim=256, num_heads=4).load(),
+    "UNet": AttentionUNet("AttentionUNet8_8600", depth=3).load(),
+    #"ViT": ViT("ViT3_2399", num_classes=9, max_tokens=128, layers=3, embed_dim=256, num_heads=4).load(),
+    "ViT": ViT("ViT3_799", num_classes=9, max_tokens=128, layers=3, embed_dim=256, num_heads=4).load(),
   }
   warmup = models["UNet"].batch_inference(np.random.randint(0, 256, size=(32*10, 32*10, 3), dtype=np.uint8), chunk_size=32)
   print(f"UNet warmup.shape: {warmup.shape}")
-  warmup = models["ViT"]([np.random.randint(0, 2, size=(64,32,32,3), dtype=np.uint8)])
+  warmup = models["ViT"]([np.random.randint(0, 2, size=(64,32,32,3), dtype=np.int64)])
   warmup = warmup.argmax(axis=1).cast(dtypes.uint8).numpy()[0]
   print(f"ViT warmup: {warmup}")
 

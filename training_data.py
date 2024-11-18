13c9b8e98c9a34bc22957fae8e7966370136f712
hooved <172129504+hooved@users.noreply.github.com>
2024-11-18 08:54:39 -0500

    debug mask/path loading and fow exploration

--- training_data.py
+++ training_data.py
@@ -39,10 +39,6 @@ use multiprocessing to run training while preloading batches in parallel
 
 Data sketch:
 
-light_radius: real in game radius is an oval, height from center = 120 px, width from center = 140 px (4k resolution)
-Our masks are downsampled by 2x in each dimension from 4k, so that would be vertical_radius = 60, horizontal_radius = 70
-For simplicity, use circular light radius of 65 px when sampling mask with path
-
 mask np.ndarray; shape (y, x)
 mask_origin np.ndarray; shape (2,)
 paths Dict[path_id str, path np.ndarray] 
@@ -61,21 +57,25 @@ path np.ndarray; shape (N_points, 2)
 Tensor(batch_size, 128,...)
 """
 
-# mask-path pairs are the highest level data for training
+# mask-path pairs are the highest level unit of data for training
 # each mask-path pair is converted to a token sequence for ViT forward pass
 class MaskPath:
   def __init__(self, mask: np.ndarray, origin: np.ndarray, path: np.ndarray):
     self.mask, self.origin, self.path = mask, origin, path
+    self.tokens = None
 
   def load(self):
-    pass
+    if self.tokens is not None: return self.tokens
+    revealed = explore_map(self.mask, self.path)
+    self.tokens = tokenize_mask(revealed, self.origin)
+    return self.tokens
 
 class ViTDataLoader:
   def __init__(self, data_dir, test_layout_split:int=1):
     self.data_dir = data_dir
     # We are using precomputed masks
     masks = set(glob.glob(os.path.join(data_dir, "*", "*", "*_mask.png"))) 
-    self.data = defaultdict(defaultdict(list))
+    self.data = defaultdict(lambda: defaultdict(list))
     self.test_layout_split = test_layout_split
 
     for mask_fp in masks:
@@ -94,24 +94,30 @@ class ViTDataLoader:
       layout_id = int(os.path.relpath(layout_dir, data_dir))
 
       # currently masks were saved with values of 0 or 255 for easy visualization
-      mask = (np.array(Image.open(mask_fp)) / 255).astype(np.bool)
+      mask = (np.array(Image.open(mask_fp)) / 255).astype(np.uint8)
       assert len(mask.shape) == 2
       # TODO: clean up filename patterns, should just be loading instance_dir/origin.npz
       #origin = np.load(os.path.join(instance_dir, ""))
-      origin = np.load(os.path.join(mask.split("_mask.png")[0], "_origin.npz"))
+      origin = np.load(f"{mask_fp.split('_mask.png')[0]}_origin.npz")['data']
       assert len(origin.shape) == 1 and origin.shape[0] == 2
+      origin = tuple(int(x) for x in origin)
 
       for path in paths:
         assert len(path.shape) == 2
         assert path.shape[1] == 2
-        self.data[layout_id][instance_id].append(MaskPath(mask, origin, path))
+        mp = MaskPath(mask, origin, path)
+        # TODO: load later with multiprocessing if time consuming
+        #t1 = time.perf_counter()
+        #mp.load()
+        #print(f"mp.load() elapsed: {(time.perf_counter() - t1):.4f}")
+        self.data[layout_id][instance_id].append(mp)
 
     self.train_test_split()
 
   # split along layout instance mask axis (which is a higher level than paths through a layout instance)
   def train_test_split(self):
     self.train_data = self.data.copy()
-    self.test_data = {}
+    self.test_data = defaultdict(lambda: defaultdict(list))
     for layout_id in self.train_data:
       for _ in range(self.test_layout_split):
         instance_id = random.choice(list(self.train_data[layout_id].keys()))
@@ -345,6 +351,35 @@ def tokenize_minimap(minimap: np.ndarray, origin: np.ndarray, model):
   tokens = get_tokens(patches)
   tokens = distance_sort(tokens)
   return tokens, minimap
+
+# Simulate exploring fog of war on a map, following given path
+
+# light_radius: real in game radius is an oval, height from center = 120 px, width from center = 140 px (4k resolution)
+# Our masks are downsampled by 2x in each dimension from 4k, so that would be vertical_radius = 60, horizontal_radius = 70
+# For simplicity, use circular light radius of 65 px when sampling mask with path
+def explore_map(no_fog_map: np.ndarray, path: np.ndarray, light_radius=65):
+  # add fog of war over entire map
+  revealed = np.zeros_like(no_fog_map)
+  lr_squared = light_radius**2
+  # For each path point, reveal map features within light_radius
+  for y, x in path:
+    # Define the bounding box for the circle
+    x_min = max(x - light_radius, 0)
+    x_max = min(x + light_radius + 1, no_fog_map.shape[1])
+    y_min = max(y - light_radius, 0)
+    y_max = min(y + light_radius + 1, no_fog_map.shape[0])
+    # Iterate over the bounding box and set pixels within the circle radius
+    for i in range(y_min, y_max):
+      for j in range(x_min, x_max):
+        # Check if the pixel is within the circular radius
+        if (i - y)**2 + (j - x)**2 <= lr_squared:
+          revealed[i, j] = no_fog_map[i, j]
+  return revealed
+
+def tokenize_mask(mask: np.ndarray, origin: np.ndarray):
+  patches = get_patches(mask, origin)
+  tokens = get_tokens(patches)
+  return tokens
 
 if __name__=="__main__":
 
